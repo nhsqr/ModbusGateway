2079d3e79e32abff70c4af3ba42526e92d9853a0
Niki <nhsqr.mail@gmail.com>
2026-08-19 13:02:53 +0300

    Translate parity/flow-control labels and Listen/Target IP labels to English

--- modbusrtuwidget.cpp
+++ modbusrtuwidget.cpp
@@ -1,39 +1,55 @@
-﻿#include "modbusrtuwidget.h"
+#include "modbusrtuwidget.h"
 #include "ui_modbusrtuwidget.h"
 
-#include <QtSerialPort/QSerialPortInfo>
+#include <QSerialPortInfo>
 
-#include <QSysInfo>
-
-ModbusRtuWidget::ModbusRtuWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::ModbusRtuWidget),
-    m_StopBits({
-        { QSerialPort::OneStop, tr("1") },
-        { QSerialPort::OneAndHalfStop, tr("1.5") },
-        { QSerialPort::TwoStop, tr("2") },
-    }),
-    m_Parity({
-        { QSerialPort::NoParity, tr("无校验") },
-        { QSerialPort::EvenParity, tr("偶校验") },
-        { QSerialPort::OddParity, tr("奇校验") },
-        { QSerialPort::SpaceParity, tr("空格校验") },
-        { QSerialPort::MarkParity, tr("标记校验") }
-    }),
-    m_FlowCtrl({
-        { QSerialPort::NoFlowControl, tr("无流控") },
-        { QSerialPort::HardwareControl, tr("硬流控(RTS/CTS)") },
-        { QSerialPort::SoftwareControl, tr("软流控(XON/XOFF)") }
-    })
+ModbusRtuWidget::ModbusRtuWidget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::ModbusRtuWidget)
 {
     ui->setupUi(this);
 
-    setSerialPortNames();
-    setBaudRate();
-    setDataBits();
-    setStopBits();
-    setParity();
-    setFlowControl();
+    const QList<QPair<QSerialPort::Parity, QString>> parity_list = {
+        { QSerialPort::NoParity, tr("None") },
+        { QSerialPort::EvenParity, tr("Even") },
+        { QSerialPort::OddParity, tr("Odd") },
+        { QSerialPort::SpaceParity, tr("Space") },
+        { QSerialPort::MarkParity, tr("Mark") }
+    };
+    const QList<QPair<QSerialPort::FlowControl, QString>> flow_list = {
+        { QSerialPort::NoFlowControl, tr("None") },
+        { QSerialPort::HardwareControl, tr("Hardware (RTS/CTS)") },
+        { QSerialPort::SoftwareControl, tr("Software (XON/XOFF)") }
+    };
+
+    for(const auto &p : parity_list){
+        ui->comboBox_Parity->addItem(p.second, static_cast<int>(p.first));
+    }
+    for(const auto &f : flow_list){
+        ui->comboBox_FlowCtrl->addItem(f.second, static_cast<int>(f.first));
+    }
+
+    ui->comboBox_DataBits->addItem("5", QSerialPort::Data5);
+    ui->comboBox_DataBits->addItem("6", QSerialPort::Data6);
+    ui->comboBox_DataBits->addItem("7", QSerialPort::Data7);
+    ui->comboBox_DataBits->addItem("8", QSerialPort::Data8);
+    ui->comboBox_DataBits->setCurrentIndex(3);
+
+    ui->comboBox_StopBits->addItem("1", QSerialPort::OneStop);
+    ui->comboBox_StopBits->addItem("1.5", QSerialPort::OneAndHalfStop);
+    ui->comboBox_StopBits->addItem("2", QSerialPort::TwoStop);
+
+    const QList<qint32> baud_rates = QSerialPortInfo::standardBaudRates();
+    for(qint32 baud : baud_rates){
+        ui->comboBox_BaudRate->addItem(QString::number(baud), baud);
+    }
+    int idx = ui->comboBox_BaudRate->findData(9600);
+    if(idx >= 0){
+        ui->comboBox_BaudRate->setCurrentIndex(idx);
+    }
+
+    refresh_serial_ports();
+    connect(ui->btn_clear_log, &QPushButton::clicked, this, &ModbusRtuWidget::sig_clear_log_requested);
 }
 
 ModbusRtuWidget::~ModbusRtuWidget()
@@ -41,90 +57,23 @@ ModbusRtuWidget::~ModbusRtuWidget()
     delete ui;
 }
 
-void ModbusRtuWidget::on_btn_clear_log_clicked()
+void ModbusRtuWidget::refresh_serial_ports()
 {
-    emit sig_clear_log_requested();
-}
-
-void ModbusRtuWidget::setSerialPortNames()
-{
-    QStringList portNames;
-    const auto ports = QSerialPortInfo::availablePorts();
-    for (const QSerialPortInfo& portInfo : ports){
-        portNames.append(portInfo.portName());
-    }
-    std::sort(portNames.begin(), portNames.end());
     ui->comboBox_SerialName->clear();
-    ui->comboBox_SerialName->addItems(portNames);
-}
-
-void ModbusRtuWidget::setBaudRate()
-{
-    QList<qint32> baudRates = QSerialPortInfo::standardBaudRates();
-    std::sort(baudRates.begin(), baudRates.end());
-    ui->comboBox_BaudRate->clear();
-    for (int i = 0; i < baudRates.count(); i++) {
-        const QString baudRate = QString::number(baudRates.at(i));
-        ui->comboBox_BaudRate->addItem(baudRate, baudRates.at(i));
-    }
-    const int defaultIndex = ui->comboBox_BaudRate->findData(9600);
-    if(defaultIndex >= 0){
-        ui->comboBox_BaudRate->setCurrentIndex(defaultIndex);
+    const auto ports = QSerialPortInfo::availablePorts();
+    for(const QSerialPortInfo &info : ports){
+        ui->comboBox_SerialName->addItem(info.portName());
     }
 }
 
-void ModbusRtuWidget::setDataBits()
+modbus_rtu_worker::serial_params ModbusRtuWidget::get_params() const
 {
-    QList<QSerialPort::DataBits> dataBits;
-    dataBits << QSerialPort::Data5 << QSerialPort::Data6 << QSerialPort::Data7 << QSerialPort::Data8;
-    std::sort(dataBits.begin(), dataBits.end());
-    ui->comboBox_DataBits->clear();
-    for (int i = 0; i < dataBits.count(); i++) {
-        const QString dataBit = QString::number(dataBits.at(i));
-        ui->comboBox_DataBits->addItem(dataBit, static_cast<int>(dataBits.at(i)));
-    }
-    const int defaultIndex = ui->comboBox_DataBits->findData(static_cast<int>(QSerialPort::Data8));
-    if(defaultIndex >= 0){
-        ui->comboBox_DataBits->setCurrentIndex(defaultIndex);
-    }
-}
-
-void ModbusRtuWidget::setStopBits()
-{
-    ui->comboBox_StopBits->clear();
-    for (auto it = m_StopBits.begin(); it != m_StopBits.end(); ++it) {
-        // 非windows平台上不支持停止位为1.5
-        if (QSysInfo::kernelType() != "winnt" && it->first == QSerialPort::OneAndHalfStop) {
-            continue;
-        }
-        ui->comboBox_StopBits->addItem(it->second, static_cast<int>(it->first));
-    }
-}
-
-void ModbusRtuWidget::setFlowControl()
-{
-    ui->comboBox_FlowCtrl->clear();
-    for (auto it = m_FlowCtrl.begin(); it != m_FlowCtrl.end(); ++it) {
-        ui->comboBox_FlowCtrl->addItem(it->second, static_cast<int>(it->first));
-    }
-}
-
-void ModbusRtuWidget::setParity()
-{
-    ui->comboBox_Parity->clear();
-    for (auto it = m_Parity.begin(); it != m_Parity.end(); ++it) {
-        ui->comboBox_Parity->addItem(it->second, static_cast<int>(it->first));
-    }
-}
-
-QStringList ModbusRtuWidget::get_params() const
-{
-    QStringList params;
-    params << ui->comboBox_SerialName->currentText()
-           << ui->comboBox_BaudRate->currentData().toString()
-           << ui->comboBox_DataBits->currentData().toString()
-           << ui->comboBox_StopBits->currentData().toString()
-           << ui->comboBox_Parity->currentData().toString()
-           << ui->comboBox_FlowCtrl->currentData().toString();
-    return params;
+    modbus_rtu_worker::serial_params p;
+    p.port_name = ui->comboBox_SerialName->currentText();
+    p.baud_rate = ui->comboBox_BaudRate->currentData().toInt();
+    p.data_bits = static_cast<QSerialPort::DataBits>(ui->comboBox_DataBits->currentData().toInt());
+    p.stop_bits = static_cast<QSerialPort::StopBits>(ui->comboBox_StopBits->currentData().toInt());
+    p.parity = static_cast<QSerialPort::Parity>(ui->comboBox_Parity->currentData().toInt());
+    p.flow_control = static_cast<QSerialPort::FlowControl>(ui->comboBox_FlowCtrl->currentData().toInt());
+    return p;
 }
--- modbustcpwidget.cpp
+++ modbustcpwidget.cpp
@@ -1,19 +1,47 @@
-﻿#include "modbustcpwidget.h"
+#include "modbustcpwidget.h"
 #include "ui_modbustcpwidget.h"
-#include "modbusassistant.h"
 
 #include <QNetworkInterface>
-#include <QtAlgorithms>
+#include <QRegularExpression>
+#include <QRegularExpressionValidator>
+#include <algorithm>
 
-ModbusTcpWidget::ModbusTcpWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::ModbusTcpWidget)
+ModbusTcpWidget::ModbusTcpWidget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::ModbusTcpWidget)
 {
     ui->setupUi(this);
-    ui->comboBox_IPAddr->setEditable(true);
-    setIPAddr();
-    setPort();
-    set_gateway_mode(GatewayMode::TcpToRtu);
+
+    QStringList ip_list;
+    const QList<QNetworkInterface> interfaces = QNetworkInterface::allInterfaces();
+    for(const QNetworkInterface &iface : interfaces){
+        // Get all IP address entries on this interface
+        const QList<QNetworkAddressEntry> entries = iface.addressEntries();
+        for(const QNetworkAddressEntry &entry : entries){
+            const QHostAddress ip = entry.ip();
+            if(ip.protocol() == QAbstractSocket::IPv4Protocol && !ip.isLoopback()){
+                // Add IPv4 addresses to the ComboBox
+                ip_list << ip.toString();
+            }
+        }
+    }
+    ip_list << "127.0.0.1" << "0.0.0.0";
+    ip_list.removeDuplicates();
+    std::sort(ip_list.begin(), ip_list.end());
+    ui->comboBox_IPAddr->addItems(ip_list);
+    int idx = ui->comboBox_IPAddr->findText("0.0.0.0");
+    if(idx >= 0){
+        ui->comboBox_IPAddr->setCurrentIndex(idx);
+    }
+
+    // Create a regular expression matching port numbers
+    QRegularExpression re("^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$");
+    // Create a QRegularExpressionValidator to restrict input
+    auto *validator = new QRegularExpressionValidator(re, this);
+    // Set the validator on the QLineEdit
+    ui->lineEdit_Port->setValidator(validator);
+
+    connect(ui->btn_clear_log, &QPushButton::clicked, this, &ModbusTcpWidget::sig_clear_log_requested);
 }
 
 ModbusTcpWidget::~ModbusTcpWidget()
@@ -21,67 +49,21 @@ ModbusTcpWidget::~ModbusTcpWidget()
     delete ui;
 }
 
-void ModbusTcpWidget::on_btn_clear_log_clicked()
-{
-    emit sig_clear_log_requested();
-}
-
-void ModbusTcpWidget::setIPAddr()
-{
-    QList<QString> ipv4Addresses;
-    foreach (QNetworkInterface interface, QNetworkInterface::allInterfaces())
-    {
-        // 获取此接口上的所有IP地址条目
-        foreach (QNetworkAddressEntry entry, interface.addressEntries()) {
-            if (entry.ip().protocol() == QAbstractSocket::IPv4Protocol) {
-                // 将IPv4地址添加到ComboBox中
-                ipv4Addresses.append(entry.ip().toString());
-            }
-        }
-    }
-
-    #if (QT_VERSION < QT_VERSION_CHECK(5, 15, 0))
-        // 在Qt 5.15或更高版本中，qSort()方法被标记为已弃用。因此，在这些版本中使用std::sort()算法。
-        qSort(ipv4Addresses);
-    #else
-        // 在Qt 5.15或更高版本中，使用std::sort()算法。
-        std::sort(ipv4Addresses.begin(), ipv4Addresses.end());
-    #endif
-    ui->comboBox_IPAddr->clear();
-    foreach (QString ipv4Address, ipv4Addresses) {
-        ui->comboBox_IPAddr->addItem(ipv4Address);
-    }
-
-    return;
-}
-
-void ModbusTcpWidget::setPort()
-{
-    // 创建一个匹配端口号规则的正则表达式
-    QRegularExpression regex(ModbusAssistant::m_regExp4PortNumber);
-    // 创建一个QRegularExpressionValidator对象，用于限制输入
-    QRegularExpressionValidator *validator = new QRegularExpressionValidator(regex, this);
-    // 将验证器设置为QLineEdit的输入验证器
-    ui->lineEdit_Port->setValidator(validator);
-    return;
-}
-
-QStringList ModbusTcpWidget::get_params() const
-{
-    QStringList params;
-    params << ui->comboBox_IPAddr->currentText()
-           << ui->lineEdit_Port->text();
-    return params;
-}
-
 void ModbusTcpWidget::set_gateway_mode(GatewayMode mode)
 {
     if(mode == GatewayMode::RtuToTcp){
-        ui->label_IPAddr->setText(tr("目标IP："));
-        ui->label_Port->setText(tr("目标端口："));
+        ui->label_IPAddr->setText(tr("Target IP:"));
+        ui->label_Port->setText(tr("Target Port:"));
         return;
     }
+    ui->label_IPAddr->setText(tr("Listen IP:"));
+    ui->label_Port->setText(tr("Listen Port:"));
+}
 
-    ui->label_IPAddr->setText(tr("监听IP："));
-    ui->label_Port->setText(tr("监听端口："));
+modbus_tcp_worker::tcp_params ModbusTcpWidget::get_params() const
+{
+    modbus_tcp_worker::tcp_params p;
+    p.ip = ui->comboBox_IPAddr->currentText();
+    p.port = ui->lineEdit_Port->text().toUShort();
+    return p;
 }
